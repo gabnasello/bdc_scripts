dc1d063490c6d4299d34c3c851081fed623ded5f
gabnasello <gabriele.nasello@gmail.com>
2024-05-15 15:04:39 +0200

    autocomplete gpu images

--- bdstart-completion.bash
+++ bdstart-completion.bash
@@ -1,3 +1,3 @@
 #/usr/bin/env bash
 
-complete -W "datascience-env sc-r-env sc-py-env fiji-env flowcyto-env napari-env flowcyto-env commbini-env paraview-env slicer-env" bdstart
+complete -W "datascience-env datascience-env-gpu sc-r-env sc-py-env sc-py-env-gpu fiji-env flowcyto-env napari-env flowcyto-env commbini-env paraview-env slicer-env" bdstart
